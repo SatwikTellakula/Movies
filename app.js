57561fcbc412c952face496183225d112fafc74d
SatwikTellakula <119725034+SatwikTellakula@users.noreply.github.com>
2022-12-10 19:46:32 +0530

    git commit -m "solution

--- app.js
+++ app.js
@@ -65,3 +65,55 @@ app.post("/movies/", async (request, response) => {
   await database.run(postMovieQuery);
   response.send("Movie Successfully Added");
 });
+app.get("/movies/:movieId/", async (request, response) => {
+  const { movieId } = request.params;
+  const getMovieByIdQuery = `
+    select * from movie where movie_id=${movieId};
+    `;
+  const movie = await database.get(getMovieByIdQuery);
+  response.send(convertMovieDbObjectToResponseObject(movie));
+});
+
+app.put("/movies/:movieId/", async (request, response) => {
+  const { directorId, movieName, leadActor } = request.body;
+  const { movieId } = request.params;
+  const updateMovieByIdQuery = `
+    UPDATE movie SET director_id=${directorId},
+    movie_name='${movieName}',
+    lead_actor='${leadActor}'
+    where movie_id=${movieId};
+    `;
+  await database.run(updateMovieByIdQuery);
+  response.send("Movie Details Updated");
+});
+app.delete("/movies/:movieId/", async (request, response) => {
+  const { movieId } = request.params;
+  const deleteMovieByIdQuery = `
+    DELETE FROM movie where movie_id=${movieId};
+    `;
+  await database.run(deleteMovieByIdQuery);
+  response.send("Movie Removed");
+});
+app.get("/directors/", async (request, response) => {
+  const getDirectors = `
+    select * from director;
+    `;
+  const directorsArray = await database.all(getDirectors);
+  response.send(
+    directorsArray.map((eachDirector) => {
+      convertDirectorDbObjectToResponseObject(eachDirector);
+    })
+  );
+});
+app.get("/directors/:directorId/movies/", async (request, response) => {
+  const { directorId } = request.params;
+  const getDirectorByIdQuery = `
+    select movie_name from movie 
+    where director_id='${directorId}';
+    `;
+  const moviesArray = await database.all(getDirectorByIdQuery);
+  response.send(
+    moviesArray.map((eachMovie) => ({ movieName: eachMovie.movie_name }))
+  );
+});
+module.exports = app;
